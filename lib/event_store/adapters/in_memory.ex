c8d989fc3411eb4893be7a98746d0d4e11511461
Mario Uher <uher.mario@gmail.com>
2021-10-10 17:28:11 +0200

    Refactor in-memory adapter

--- lib/event_store/adapters/in_memory.ex
+++ lib/event_store/adapters/in_memory.ex
@@ -1,10 +1,10 @@
 defmodule EventStore.Adapters.InMemory do
   @behaviour EventStore.Adapter
 
-  use GenServer
+  use Agent
 
   def start_link(_) do
-    GenServer.start_link(__MODULE__, [], name: __MODULE__)
+    Agent.start_link(fn -> [] end, name: __MODULE__)
   end
 
   @impl true
@@ -14,46 +14,21 @@ defmodule EventStore.Adapters.InMemory do
       |> Ecto.Changeset.put_change(:id, Ecto.UUID.generate())
       |> Ecto.Changeset.apply_changes()
 
-    GenServer.call(__MODULE__, {:dispatch, event})
+    Agent.update(__MODULE__, &[event | &1])
+
+    {:ok, event}
   end
 
   @impl true
   def stream(aggregate_id) do
-    GenServer.call(__MODULE__, {:stream, aggregate_id})
+    Agent.get(__MODULE__, & &1)
+    |> Enum.filter(&(&1.aggregate_id == aggregate_id))
+    |> Enum.reverse()
   end
 
   @impl true
   def exists?(aggregate_id, name) do
-    GenServer.call(__MODULE__, {:exists, aggregate_id, name})
-  end
-
-  # GenServer API
-
-  @impl true
-  def init(events) do
-    {:ok, events}
-  end
-
-  @impl true
-  def handle_call({:dispatch, event}, _from, events) do
-    {:reply, {:ok, event}, [{event.aggregate_id, event} | events]}
-  end
-
-  @impl true
-  def handle_call({:stream, aggregate_id}, _from, events) do
-    stream =
-      for {event_aggregate_id, event} <- events, event_aggregate_id == aggregate_id, do: event
-
-    {:reply, Enum.reverse(stream), events}
-  end
-
-  @impl true
-  def handle_call({:exists, aggregate_id, name}, _from, events) do
-    result =
-      Enum.any?(events, fn {_, event} ->
-        event.aggregate_id == aggregate_id and event.name == name
-      end)
-
-    {:reply, result, events}
+    Agent.get(__MODULE__, & &1)
+    |> Enum.any?(&(&1.aggregate_id == aggregate_id and &1.name == name))
   end
 end

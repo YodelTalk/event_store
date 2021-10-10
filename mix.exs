844cbad4d9aec9aa1b0c25ab33bd85fe9ad3830e
Mario Uher <uher.mario@gmail.com>
2021-10-10 13:45:05 +0200

    Stop supporting Elixir v1.11

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule EventStore.MixProject do
     [
       app: :event_store,
       version: "0.1.0",
-      elixir: "~> 1.11",
+      elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps()

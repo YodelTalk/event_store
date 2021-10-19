b10de05d334ca7ca41b84824ebc52b7ae60a28a4
Mario Uher <uher.mario@gmail.com>
2021-10-19 16:14:11 +0200

    Adapt in-memory adapter to the new changes

--- lib/event_store/adapters/in_memory.ex
+++ lib/event_store/adapters/in_memory.ex
@@ -1,17 +1,32 @@
 defmodule EventStore.Adapters.InMemory do
+  @moduledoc """
+  **Don't use this adapter in production!** Its currently not optimized at all. It's
+  main purpose is for testing and to demonstrate how easy you can build your own
+  adapter.
+  """
+
   @behaviour EventStore.Adapter
 
   use Agent
 
   def start_link(_) do
+    # TODO: Change the internal storage to something more performant than a list.
     Agent.start_link(fn -> [] end, name: __MODULE__)
   end
 
   @impl true
   def insert(changeset) do
+    # TODO: Improve the performance of the next_aggregate_version.
+    next_aggregate_version =
+      changeset.changes.aggregate_id
+      |> stream()
+      |> Enum.count()
+      |> then(&(&1 + 1))
+
     event =
       changeset
       |> Ecto.Changeset.put_change(:id, Ecto.UUID.generate())
+      |> Ecto.Changeset.put_change(:aggregate_version, next_aggregate_version)
       |> Ecto.Changeset.apply_changes()
 
     Agent.update(__MODULE__, &[event | &1])

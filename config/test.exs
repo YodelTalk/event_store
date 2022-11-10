d88afa88c185d1ba718bf04fed322155b6e445ec
Mario Uher <uher.mario@gmail.com>
2022-11-10 17:27:35 +0100

    Stop testing impossible scenarios

--- config/test.exs
+++ config/test.exs
@@ -4,7 +4,4 @@ config :event_store,
   adapter:
     System.get_env("EVENT_STORE_ADAPTER", "Elixir.EventStore.Adapters.InMemory")
     |> String.to_atom(),
-  pub_sub:
-    System.get_env("EVENT_STORE_PUB_SUB", "Elixir.EventStore.PubSub.Registry")
-    |> String.to_atom(),
   sync_timeout: 100
